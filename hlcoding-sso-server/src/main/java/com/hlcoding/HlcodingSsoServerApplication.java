10476ae2002620958f150faa7750b35a9f97c43b
hhhh <407933747>
2022-07-05 21:13:07 +0800

    工程初始化

---
+++ hlcoding-sso-server/src/main/java/com/hlcoding/HlcodingSsoServerApplication.java
@@ -0,0 +1,13 @@
+package com.hlcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlcodingSsoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HlcodingSsoServerApplication.class, args);
+    }
+
+}
